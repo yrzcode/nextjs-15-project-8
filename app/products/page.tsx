f6e4da99c46e69849e2c2b1b43741cde53072590
rz_yang <yrzcode@hotmail.com>
2025-04-03 21:51:22 +0900

    fix build type error

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,9 +1,9 @@
 import ProductsContainer from "@/components/products/ProductsContainer";
 
-export default function ProductsPage({
+export default async function ProductsPage({
 	searchParams,
-}: { searchParams: { layout?: string; search?: string } }) {
-	const layout = searchParams.layout ?? "grid";
-	const search = searchParams.search ?? "";
+}: { searchParams: Promise<{ layout?: string; search?: string }> }) {
+	const layout = (await searchParams).layout ?? "grid";
+	const search = (await searchParams).search ?? "";
 	return <ProductsContainer layout={layout} search={search} />;
 }
