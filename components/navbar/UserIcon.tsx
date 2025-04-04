6b89ce154b75449f19b8e1c63d1f6967d5960066
rz_yang <yrzcode@hotmail.com>
2025-04-04 16:10:02 +0900

    fix build error

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,8 +1,7 @@
-import { auth, currentUser } from "@clerk/nextjs/server";
+import { currentUser } from "@clerk/nextjs/server";
 import { LuUser } from "react-icons/lu";
 
 export default async function UserIcon() {
-	// const {userId} = auth();
 	const user = await currentUser();
 	const profileImage = user?.imageUrl;
 	if (profileImage) {
