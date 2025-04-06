c07e1d866985755c0b8967dd38ef34d8098130fc
rz_yang <yrzcode@hotmail.com>
2025-04-07 03:11:05 +0900

    fix build error

--- app/admin/products/create/page.tsx
+++ app/admin/products/create/page.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button";
 import { faker } from "@faker-js/faker";
 import FormInput from "@/components/form/FormInput";
 import FormContainer from "@/components/form/FormContainer";
