29ce4bc85208d0242e2a580816e6895ce19ab5c7
rz_yang <yrzcode@hotmail.com>
2025-04-08 22:34:28 +0900

    Fix build error

--- components/reviews/Rating.tsx
+++ components/reviews/Rating.tsx
@@ -4,7 +4,7 @@ export default function Rating({ rating }: { rating: number }) {
 	const stars = Array.from({ length: 5 }, (_, i) => i < rating);
 	return (
 		<div className="flex items-center gap-x-1">
-			{stars.map((isFilled, i) => {
+			{stars.map((isFilled) => {
 				const className = `w-3 h-3 ${isFilled ? "text-primary" : "text-gray-400"}`;
 				return isFilled ? (
 					<FaStar className={className} key={crypto.randomUUID()} />
